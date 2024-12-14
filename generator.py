954c02b4eb09e00877593dc8a60402148305ea72
Truff1e <truffle@vacasound.com>
2024-12-14 16:40:48 -0500

    1.7.0
    - Updated Texture Pack Compatibility

--- generator.py
+++ generator.py
@@ -36,7 +36,7 @@ def write_getgoals_function(path, goals):
 
 def write_advancement_tree(path, goals, letters):
     file = open(f'{path}/data/lockout/advancement/board/root.json', 'w')
-    file.write('{\n"display": {\n"icon": {\n"id":"minecraft:ominous_trial_key","components": {"minecraft:custom_model_data": 1}\n},\n"title": "Lockout Board",\n"description": "Generated with the Truff1e Lockout Board Generator", \n"background": "minecraft:textures/block/stone.png",\n"frame": "task", \n"announce_to_chat": false, \n"hidden": false},\n"criteria": {"trigger": {"trigger": "minecraft:impossible"}}}')
+    file.write('{\n"display": {\n"icon": {\n"id":"minecraft:ominous_trial_key","components": {"minecraft:custom_model_data": {"strings": ["25adv"]}}\n},\n"title": "Lockout Board",\n"description": "Generated with the Truff1e Lockout Board Generator", \n"background": "minecraft:textures/block/stone.png",\n"frame": "task", \n"announce_to_chat": false, \n"hidden": false},\n"criteria": {"trigger": {"trigger": "minecraft:impossible"}}}')
     file.close()
     for i in range(len(goals)):
         letter, num = goals[i][1].strip('123456789'), int(goals[i][1].strip('abcdefghij'))
